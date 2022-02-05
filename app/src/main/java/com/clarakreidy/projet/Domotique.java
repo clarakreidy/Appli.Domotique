41d28cbec1f9893b5c56186b43ba62a600a64a0f
Clara Elkreidy <clarakreidy@hotmail.com>
2022-02-05 14:17:00 +0100

    domotique model and adapter

---
+++ app/src/main/java/com/clarakreidy/projet/Domotique.java
@@ -0,0 +1,31 @@
+package com.clarakreidy.projet;
+
+public class Domotique {
+    private Integer id;
+    private String name;
+    private String type;
+    private String picture;
+
+    public Domotique(Integer id, String name, String type, String picture) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+        this.picture = picture;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getPicture() {
+        return picture;
+    }
+}
---
+++ app/src/main/java/com/clarakreidy/projet/DomotiqueAdapter.java
@@ -0,0 +1,82 @@
+package com.clarakreidy.projet;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DomotiqueAdapter extends ArrayAdapter<Domotique> {
+    //the list values in the List of type hero
+    ArrayList<Domotique> domotiques;
+
+    //activity context
+    Context context;
+
+    //the layout resource file for the list items
+    int resource;
+
+    public DomotiqueAdapter(@NonNull Context context, int resource, @NonNull ArrayList<Domotique> objects) {
+        super(context, resource, objects);
+        this.context = context;
+        this.resource = resource;
+        this.domotiques = objects;
+    }
+
+    //this will return the ListView Item as a View
+    @NonNull
+    @Override
+    public View getView(final int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+        //we need to get the view of the xml for our list item
+        //And for this we need a layout inflater
+        LayoutInflater layoutInflater = LayoutInflater.from(context);
+
+        //getting the view
+        View view = layoutInflater.inflate(resource, null, false);
+
+        //getting the view elements of the list from the view
+        ImageView imageView = view.findViewById(R.id.room_img);
+        TextView textName = view.findViewById(R.id.room_name);
+
+        //getting the hero of the specified position
+        Domotique domotique = domotiques.get(position);
+
+        //adding values to the list item
+        Glide.with(context)
+                .asBitmap()
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .load("https://myhouse.lesmoulinsdudev.com/" + domotique.getPicture())
+                .listener(new RequestListener<Bitmap>() {
+                    @Override
+                    public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Bitmap> target, boolean isFirstResource) {
+                        return false;
+                    }
+
+                    @Override
+                    public boolean onResourceReady(Bitmap resource, Object model, Target<Bitmap> target, DataSource dataSource, boolean isFirstResource) {
+                        return false;
+                    }
+                }).into(imageView);
+        textName.setText(domotique.getName());
+
+        //finally returning the view
+        return view;
+    }
+}
